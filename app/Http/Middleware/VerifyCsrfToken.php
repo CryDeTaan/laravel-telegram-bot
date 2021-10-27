281325d36a918e68f62d8501e1a32d643dd5c763
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-10-27 05:55:20 +0100

    Removed TODO comment.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,11 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        // TODO: It would be nice to read this from
-        //  config. Maybe something like this:
-        //  https://laracasts.com/discuss/channels/laravel/define-except-in-middleware-using-env
         '/telegram/webhook/*',
-
-        // ...
     ];
 }
